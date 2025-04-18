4d533992bca6222786ef01eb5955a3b212da722e
its <timotej.kobe@gmail.com>
2025-04-18 13:24:56 +0200

    removed gunicorn because it doesn't work.

--- main/main.py
+++ main/main.py
@@ -6,43 +6,9 @@ try:
     button.activation_function = wifi_config.start_hotspot
 except ModuleNotFoundError:
     pass
-from sys import platform
-do_gunicorn = False
-if do_gunicorn and platform == 'linux':
-    print("Detected Linux, Preparing gunicorn")
-    try:
-        import gunicorn.app.base
-    except ModuleNotFoundError:
-        print("Gunicorn not installed!")
-        do_gunicorn = False
 
-    class StandaloneApplication(gunicorn.app.base.BaseApplication):
-        def __init__(self, app, options=None):
-            self.options = options or {}
-            self.application = app
-            super().__init__()
-
-        def load_config(self):
-            config = {key: value for key, value in self.options.items()
-                    if key in self.cfg.settings and value is not None}
-            for key, value in config.items():
-                self.cfg.set(key.lower(), value)
-
-        def load(self):
-            return self.application
-
+movement_ = movement.Movement()
+control.control_function = movement_.move_robot
 
 if __name__ == "__main__":
-    movement_ = movement.Movement()
-    control.control_function = movement_.move_robot
-
-    if do_gunicorn and platform == 'linux':
-        options = {
-            'bind': '%s:%s' % ('0.0.0.0', '5000'),
-            'workers': 2,
-            'timeout': 120,
-        }
-        StandaloneApplication(control.app, options).run()
-    else:
-        print("Starting flask test mode as it's not linux this yes")
-        control.app.run(host='0.0.0.0', port=5000, debug=False)
+    control.app.run(host='0.0.0.0', port=5000, debug=False)
