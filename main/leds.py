543a3788635d27f0270d36abc453fb401c275540
tkobe28 <timotej.kobe@gmail.com>
2025-05-10 22:02:11 +0200

    Improved leds.py, added test mode

--- main/leds.py
+++ main/leds.py
@@ -1,36 +1,70 @@
-import RPi.GPIO as GPIO
 import time
+try:
+    import RPi.GPIO as GPIO
 
-# red, green, blue
-led_pins = [27, 17, 22]  # Adjust as needed
+    leds = {  # (state, pin_number)
+        "red": (False, 27),
+        "green": (False, 17),
+        "blue": (False, 22),
+    }
 
-GPIO.setmode(GPIO.BCM)
+    GPIO.setmode(GPIO.BCM)
 
-# Setup pins as output
-for pin in led_pins:
-    GPIO.setup(pin, GPIO.OUT)
+    # Setup pins as output
+    for state, pin in leds:
+        GPIO.setup(pin, GPIO.OUT)
 
-def turn_led(which: str | int, on: bool):
-    if type(which) is str:
-        if which == "red":
-            which = 0
-        elif which == "green":
-            which = 1
-        else:  # default
-            which = 2
-    
-    GPIO.output(pin, GPIO.HIGH if on else GPIO.LOW)
+    def turn_led(which: str, on: bool | None = None):
+        state, pin = leds[which]
+
+        if on is None:
+            on = not state
+
+        GPIO.output(pin, GPIO.HIGH if on else GPIO.LOW)
+
+        print(f"Turned the {which} LED {'on' if on else 'off'}." if not on == state
+              else f"The {which} LED is already {'on' if on else 'off'}.")
+
+        leds[which] = (on, pin)
 
 
-if __name__ == "__main__":
-    try:
-        while True:
-            for pin in led_pins:
-                GPIO.output(pin, GPIO.HIGH)
-                time.sleep(0.5)
-                GPIO.output(pin, GPIO.LOW)
-                time.sleep(0.2)
-    except KeyboardInterrupt:
-        pass
-    finally:
-        GPIO.cleanup()
+    if __name__ == "__main__":
+        try:
+            while True:
+                for led_name in leds.keys():
+                    turn_led(led_name)
+                    time.sleep(0.2)
+
+        except KeyboardInterrupt:
+            pass
+        finally:
+            GPIO.cleanup()
+except ImportError:
+    print("Not found libraries - using test virtual leds")
+
+    leds = {
+        "red": False,
+        "green": False,
+        "blue": False
+    }
+
+    def turn_led(which: str, on: bool | None = None):
+        state = leds[which]
+
+        if on is None:
+            on = not state
+
+        print(f"Turned the {which} LED {'on' if on else 'off'}." if not on == state
+              else f"The {which} LED is already {'on' if on else 'off'}.")
+
+        leds[which] = on
+
+    if __name__ == "__main__":
+        try:
+            while True:
+                for led_name in leds.keys():
+                    turn_led(led_name)
+                    time.sleep(0.2)
+
+        except KeyboardInterrupt:
+            pass
